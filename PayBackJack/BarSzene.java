c652196f1e7df913c74e3d8db9dcb25177a969a5
an <an@example.com>
2026-06-25 14:51:43 +0200

    UI adjustment: moved Huan up and scaled 1.5x larger

--- PayBackJack/BarSzene.java
+++ PayBackJack/BarSzene.java
@@ -35,7 +35,7 @@ public class BarSzene {
         hintergrund = new BildE(0, 0, "../Assets/Sprites/Bar/Background_640x360.png");
         hintergrund.sichtbarSetzen(false);
         
-        huanBartender = new Huan(OFFSET_X + 50, 660);
+        huanBartender = new Huan(OFFSET_X + 50, 480);
         huanBartender.sichtbarSetzen(false);
         huanBartender.sprich("Willkommen... Was darfs sein?");
         
