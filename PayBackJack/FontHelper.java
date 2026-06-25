f71538313ad0fc485863f4b98bab291fa57d55f5
an <an@example.com>
2026-06-25 14:15:10 +0200

    Update font to Tiny5

--- PayBackJack/FontHelper.java
+++ PayBackJack/FontHelper.java
@@ -6,7 +6,7 @@ import ea.edu.TextE;
  * Hilfsklasse zum zentralen Zuweisen der Pixelify Sans Schriftart auf Texte.
  */
 public class FontHelper {
-    public static final String FONT_NAME = "Pixelify Sans Regular";
+    public static final String FONT_NAME = "Tiny5 Regular";
 
     /**
      * Wendet die Schriftart auf alle uebergebenen TextE-Objekte an.
